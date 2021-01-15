28e7542ab0aff9dd1e067ffdda974fa5d2984bfd
Al-Amin <cseal.amin.09@gmail.com>
2021-01-15 11:32:40 +0600

    add permission callback for rest api

--- includes/REST/Manager.php
+++ includes/REST/Manager.php
@@ -14,31 +14,44 @@ class Manager {
 	}
 
 	public function get_vendor() {
-		register_rest_route( $this->namespace, 'vendor', [
-			'method' => 'GET',
-			'callback' => array( $this, 'migrate_vendor' ),
-		] );
+		register_rest_route( 
+			$this->namespace, 
+			'vendor', 
+			[
+				'method' => 'GET',
+				'callback' => array( $this, 'migrate_vendor' ),
+				'permission_callback' => array( $this, 'check_permission' ),
+			]
+		);
 	}
 
 	public function get_withdraw() {
 		register_rest_route( $this->namespace, 'withdraw', [
 		    'methods' => 'GET',
-		    'callback' => array( $this, 'migrate_withdraw' )
+		    'callback' => array( $this, 'migrate_withdraw' ),
+		    'permission_callback' => array( $this, 'check_permission' ),
   		] );
 	}
 
 	public function get_order() {
 		register_rest_route( $this->namespace, 'order', [
 		    'methods' => 'GET',
-		    'callback' => array( $this, 'migrate_order' )
+		    'callback' => array( $this, 'migrate_order' ),
+		    'permission_callback' => array( $this, 'check_permission' ),
   		] );
 	}
 
 	public function get_refund() {
 		register_rest_route( $this->namespace, 'refund', [
 		    'methods' => 'GET',
-		    'callback' => array( $this, 'migrate_refund' )
+		    'callback' => array( $this, 'migrate_refund' ),
+		    'permission_callback' => array( $this, 'check_permission' ),
   		] );
+	}
+
+	public function check_permission() {
+		return true;
+		// return current_user_can( 'manage_options' );
 	}
 
 	public function migrate_vendor() {
